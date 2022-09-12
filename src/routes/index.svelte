6aebbeca4c9201dc2ce8020679618b9926ae898c
Jarelle Emmanuel Payad Andulan <torten@proton.me>
2022-09-12 19:29:00 +0800

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+
+<head>
+    <title>「Home」- Torten</title>
+    <meta property="og:title" content="「Home」">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://torten.xyz">
+</head>
+
+<main>
+    
+</main>
